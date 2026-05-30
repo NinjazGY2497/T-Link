6fe89a905f7438c5a16b729e76e850c9182e18c4
NinjazGY2497 <gyagau@gmail.com>
2026-05-30 01:53:21 +0000

    technically, the oauth is part of setup also, added time and date to each entry

--- setupBP.py
+++ setupBP.py
@@ -1,13 +1,18 @@
+from datetime import datetime
 from flask import Blueprint, request, jsonify
 
 setupBP = Blueprint('setup', __name__)
+
+def timeAndDateStr() -> str:
+    now = datetime.now()
+    return f"{now.strftime('%I:%M:%S %p')} {now.strftime('%m-%d-%Y')}: "
 
 @setupBP.route('/oauth-callback')
 def oauthCallback():
     data = request.args
     print(data)
-    with open("storage/oauth-callback-data.txt", "a") as f:
-        f.write(str(data) + "\n")
+    with open("setup/OAuth/oauth-callback-data.txt", "a") as f:
+        f.write(timeAndDateStr() + str(data) + "\n")
 
     return """
         <h1>Tesla Account Connected Successfully!</h1>
