e9b447c73aa4a576d3c90dfb7caecf519a8ff635
NinjazGY2497 <gyagau@gmail.com>
2026-05-30 15:53:42 -0400

    changed order

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from BPs.getLocationsBP import getLocationsBP
 
 app = Flask(__name__)
 app.register_blueprint(setupBP, url_prefix="/setup")
-app.register_blueprint(updatePhoneLocBP)
 app.register_blueprint(getLocationsBP)
+app.register_blueprint(updatePhoneLocBP)
 
 if __name__ == "__main__":
     app.run(port="5000")
