28e3ea00e011436c1741b9cdfd688cd6e84c75b0
NinjazGY2497 <gyagau@gmail.com>
2026-05-30 01:42:42 +0000

    construct the url that the user will be redirected to (in order to authorize third party access token)

--- setup/OAuthURLBuilder.py
+++ setup/OAuthURLBuilder.py
@@ -1,8 +1,20 @@
 import urllib.parse
+import secrets
 
 baseURL = "https://auth.tesla.com/oauth2/v3/authorize"
+secretString = secrets.token_hex(16)
 parameters = {
     "response_type": "code",
-    "client_id": "2b36c057-a568-4916-a526-8e392f9d8d49",
-    "redirect_uri": "https://",
+    "client_id": "[REDACTED]",
+    "redirect_uri": "https://friendly-waddle-wr9gvwgpg6jg3gjrx-5000.app.github.dev/setup/oauth-callback", # Note: MUST MATCH IN TESLA DEVELOPER DASHBOARD!
+    "scope": "openid offline_access vehicle_device_data vehicle_location user_data",
+    "state": secretString,
+    "audience": "https://fleet-api.prd.na.vn.cloud.tesla.com",
+    
+    "prompt_missing_scopes": "true",
+    "require_requested_scopes": "true"
 }
+print(f"Used Secret String: {secretString}")
+
+URLencodedParameters = urllib.parse.urlencode(parameters)
+print(f"URL: {baseURL}?{URLencodedParameters}")
