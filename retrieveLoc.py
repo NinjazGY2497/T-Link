0afad5aca3b0d73ba12618798ab4db4c189a8f7c
NinjazGY2497 <gyagau@gmail.com>
2026-05-31 13:46:22 -0400

    Step 4 and Pre-Step 5 with supreme error handling (excluding requestTeslaEndpoint.py; still gotta do that)

--- retrieveLoc.py
+++ retrieveLoc.py
@@ -1,28 +1,53 @@
 from time import time
+from requests.exceptions import HTTPError, RequestException
+
 from security.manageTokens import loadTokens, refreshTokens
+from requestTeslaEndpoint import requestTeslaEndpoint
 
 def retrieveLoc(person: str):
     accessToken, refreshToken, expiresAt = loadTokens(person)
 
     # --- Step 3 ---
     if time() >= expiresAt - 300: # If token will expire in 300s or less
-        print("**retrieveLoc.py** - Access token expired, refreshing...")
-        accessToken, refreshToken, error = refreshTokens(person, refreshToken)
+        print("**retrieveLoc.py** - Calculated that the access token expired; refreshing...")
+        accessToken, refreshToken, refreshError = refreshTokens(person, refreshToken)
 
-        if error:
-            return {"error": str(error)}
+        if refreshError:
+            print("**retrieveLoc.py** - ERROR: Aborting retrieveLoc.py due to refresh error")
+            return {"error": str(refreshError)}
 
         print(f"**retrieveLoc.py** - Refreshed, encrypted, and stored tokens.")
 
-    return { # Mock
-        "latitude": None,
-        "longitude": None,
-        "heading": None,
-        "timestamp": None,
-        "driving_state": None, # (shift_state)
-        "online_state": None,
-        "error": None
-    }
+    # --- Step 4 ---
+    locInfo: dict
+    locError: HTTPError | RequestException | None
+    locInfo, locError = requestTeslaEndpoint(person, accessToken)
+
+    # Token Expiry or Missing Scope Perms
+    if isinstance(locError, HTTPError) and locError.response.status_code in (401, 403):
+        print("**retrieveLoc.py** - ERROR: Got a 401 or 403; refreshing tokens...")
+        accessToken, refreshToken, refreshError = refreshTokens(person, refreshToken)
+
+        if refreshError:
+            print("**retrieveLoc.py** - ERROR: Aborting retrieveLoc.py due to refresh error")
+            return {"error": str(refreshError)}
+
+        print(f"**retrieveLoc.py** - Refreshed, encrypted, and stored tokens.")
+        locInfo, locError = requestTeslaEndpoint(person, accessToken)
+
+    # If first locError was not 401/403, OR if receiving location failed for a 2nd time
+    if locError:
+        # --- Pre-Step 5 --- (Catch 408 Car is Offline error)
+        if isinstance(locError, HTTPError) and locError.response.status_code == 408:
+            print("**retrieveLoc.py** - ERROR: Got a 408; aborting because car is offline.")
+            return {"online_state": "Offline"}
+        else:
+            print("**retrieveLoc.py** - ERROR: Aborting retrieveLoc.py due to unknown location grabbing error")
+            return {"error": str(locError)}
+
+    print("**retrieveLoc.py** - Successfully retrieved location info.")
+
+    return locInfo
 
 if __name__ == "__main__":
     retrieveLoc("mom")
