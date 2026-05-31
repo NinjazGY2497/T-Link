e6da6bbed1d9a904cd276a31e7cea62b07ce40b9
NinjazGY2497 <gyagau@gmail.com>
2026-05-31 01:42:12 -0400

    added token refreshing and token saving

--- retrieveLoc.py
+++ retrieveLoc.py
@@ -1,13 +1,29 @@
 from time import time
-from security.manageTokens import unencryptTokens, refreshTokens
+from security.manageTokens import loadTokens, refreshTokens
 
 def retrieveLoc(person: str):
-    accessToken, refreshToken, expiresAt = unencryptTokens(person)
+    accessToken, refreshToken, expiresAt = loadTokens(person)
 
     # --- Step 3 ---
     if time() >= expiresAt - 300: # If token will expire in 300s or less
-        print("**retrieveLoc.py** - Access token expired; refreshing")
-        accessToken = refreshTokens(person)
+        print("**retrieveLoc.py** - Access token expired, refreshing...")
+        accessToken, refreshToken, error = refreshTokens(person, refreshToken)
+
+        if error:
+            print(f"**retrieveLoc.py** - ERROR: Failed to refresh tokens: {error}")
+            return {"error": str(error)}
+
+        print(f"**retrieveLoc.py** - Refreshed, encrypted, and stored tokens.")
+
+    return { # Mock
+        "latitude": None,
+        "longitude": None,
+        "heading": None,
+        "timestamp": None,
+        "driving_state": None, # (shift_state)
+        "online_state": None,
+        "error": None
+    }
 
 if __name__ == "__main__":
     retrieveLoc("mom")
--- security/manageTokens.py
+++ security/manageTokens.py
@@ -1,20 +1,59 @@
 import json
 from pathlib import Path
-from security.encryption import decrypt
+from time import time
+import os
+import requests
+from dotenv import load_dotenv
+
+from requests.exceptions import HTTPError, RequestException
+from security.encryption import decrypt, encrypt
+
+load_dotenv()
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+TOKEN_URL = "https://fleet-auth.prd.vn.cloud.tesla.com/oauth2/v3/token"
 
 def getTokensFilePath(person: str):
     """This specific path function was generated using Gemini 3.5"""
     scriptDir = Path(__file__).resolve().parent # ALWAYS resolves to T-Link/security/manageTokens.py
     return scriptDir.parent / "storage" / f"{person}Tokens.json"
 
-def unencryptTokens(person: str):
+def loadTokens(person: str):
     with open(getTokensFilePath(person), "r") as f:
         data = json.load(f)
         accessToken, refreshToken, expiresAt = data["access_token"], data["refresh_token"], data["expires_at"]
         return decrypt(accessToken), decrypt(refreshToken), expiresAt
 
-def refreshTokens(person: str):
-    print("[Refreshing token]")
+def saveTokens(person: str, accessToken, refreshToken, expiresAt):
+    with open(getTokensFilePath(person), "w") as f:
+        accessToken, refreshToken = encrypt(accessToken), encrypt(refreshToken)
+        json.dump({
+            "access_token": accessToken,
+            "refresh_token": refreshToken,
+            "expires_at": expiresAt
+        }, f, indent=4)
 
-if __name__ == "__main__":
-    print(unencryptTokens("mom"))
+def refreshTokens(person: str, refreshToken) -> tuple[str, str, None | Exception]:
+    body = {
+        "grant_type": "refresh_token",
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
+        "refresh_token": refreshToken,
+    }
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    try:
+        r = requests.post(TOKEN_URL, data=body, headers=headers)
+        r.raise_for_status() # Raise if error-y status code arrives
+    except (HTTPError, RequestException) as e:
+        print(f"**manageTokens.py** - ERROR: Failed to refresh tokens | Error: {type(e).__name__} {e}")
+        return None, None, e
+
+    data = r.json()
+    accessToken, refreshToken, expiresIn = data["access_token"], data["refresh_token"], data["expires_in"]
+
+    expiresAt = int(expiresIn) + int(time())
+    saveTokens(person, accessToken, refreshToken, expiresAt)
+
+    return accessToken, refreshToken, None
